11a0501a93746965542c57bb95a8065622ab6771
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2024-11-30 15:47:25 -0800

    Added basic ending panel with callback interfaces

---
+++ src/main/java/edu/gonzaga/renderer/EndPanel.java
@@ -0,0 +1,47 @@
+package edu.gonzaga.renderer;
+
+import edu.gonzaga.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The JPanel that is displayed when the game is over. Contains game statistics and buttons to restart or quit.
+ */
+public class EndPanel extends JPanel {
+    public EndPanel(EndPanelCallbacks callbacks, Player winner) {
+        super(new BorderLayout());
+
+        setBackground(Color.BLACK);
+
+        JLabel endLabel = new JLabel("GAME OVER");
+        endLabel.setForeground(Color.WHITE);
+        endLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(endLabel, BorderLayout.NORTH);
+
+        JLabel winnerLabel = new JLabel(winner.getName() + " wins!");
+        winnerLabel.setForeground(Color.WHITE);
+        winnerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(winnerLabel, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.BLACK);
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        JButton restartButton = new JButton("Restart");
+        restartButton.addActionListener(e -> callbacks.endPanelOnRestart());
+
+        JButton mainMenuButton = new JButton("Return to Main Menu");
+        mainMenuButton.addActionListener(e -> callbacks.endPanelOnMainMenu());
+
+        JButton quitButton = new JButton("Quit");
+        quitButton.addActionListener(e -> System.exit(0));
+
+        buttonPanel.add(restartButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        buttonPanel.add(mainMenuButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        buttonPanel.add(quitButton);
+    }
+}
---
+++ src/main/java/edu/gonzaga/renderer/EndPanelCallbacks.java
@@ -0,0 +1,6 @@
+package edu.gonzaga.renderer;
+
+public interface EndPanelCallbacks {
+    void endPanelOnRestart();
+    void endPanelOnMainMenu();
+}
